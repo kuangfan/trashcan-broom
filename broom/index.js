2810af794976c3a5db55065b943688fb5510bb00
BengBu-YueZhang <zhangyue_coder@163.com>
2020-05-19 21:54:19 +0800

    feat: 添加错误上报的sdk

--- broom/index.js
+++ broom/index.js
@@ -1,1 +1,16 @@
-console.log(1)
+const errorButton = document.getElementById('error-button')
+const promiseErrorButton = document.getElementById('promise-error-button')
+
+function createdError () {
+  const a = {}
+  a.b()
+}
+
+function createdPromiseError () {
+  new Promise((resolve, reject) => {
+    reject('异步错误')
+  })
+}
+
+errorButton.addEventListener('click', createdError)
+promiseErrorButton.addEventListener('click', createdPromiseError)
--- broom/public/error.collecting.js
+++ broom/public/error.collecting.js
@@ -1,1 +1,47 @@
-console.log(2)
+const TraceKit = window.TraceKit
+const errors = []
+
+let isReporting = false
+
+window.onerror = function onError (message, source, row, col) {
+  const err = {
+    message,
+    source,
+    row,
+    col
+  }
+  handleError(err)
+}
+
+window.onunhandledrejection = (err) => {
+  const err = {
+    message: err.reason,
+    source: 'async',
+    row: '',
+    col: ''
+  }
+  handleError(err)
+}
+
+function handleError (err) {
+  errors.push(err)
+  reportError()
+}
+
+function reportError () {
+  if (isReporting) {
+     return
+  }
+  isReporting = true
+  requestIdleCallback(reportErrorCallback)
+}
+
+function reportErrorCallback (deadline) {
+  isReporting = false
+  while (deadline.timeRemaining() > 0 && datas.length > 0) {
+    // http(datas.pop())
+  }
+  if (datas.length) {
+    reportError()
+  }
+}
