2dd9332be33e8637a469a2877ad489469c0407d6
BengBu-YueZhang <zhangyue_coder@163.com>
2020-05-21 15:10:18 +0800

    fead: 获取错误源码的controller

--- trashcan-server/controller/sm.controller.js
+++ trashcan-server/controller/sm.controller.js
@@ -16,12 +16,29 @@ module.exports = {
     await next()
   },
   async getSm (ctx, next) {
-    const report = ctx.request.body
+    const report = ctx.request.query
     const { source, row, col } = report
     const sourceFile = source.split('/')[source.split('/').length - 1]
     const smFiles = fs.readdirSync(smPathDir)
-    const smFile = smFiles.filter(file => new RegExp(file).test(sourceFile))
-    const smFilePath = path.resolve(__dirname, '../sm', smFile)
+    const smFile = smFiles.filter(file => file.indexOf(sourceFile) > -1)
+    const smFilePath = path.resolve(__dirname, '../sm', smFile[0])
+    const smFileDataBuffer = fs.readFileSync(smFilePath)
+    const smFileDataStr = smFileDataBuffer.toString()
+    const smFileDataObj = JSON.parse(smFileDataStr)
+    const sources = smFileDataObj.sources.map(source => source.replace(/\.[\.\/]+/g, ''))
+    const consumer = await new sourceMap.SourceMapConsumer(smFileDataStr)
+    const result = consumer.originalPositionFor({
+      line: Number(row),
+      column: Number(col)
+    })
+    const index = sources.indexOf(result.source)
+    result.content = smFileDataObj.sourcesContent[index]
+    ctx.result = {
+      msg: 'success',
+      code: 200,
+      data: result
+    }
+    await next()
   },
   async addSm (ctx, next) {
     const report = ctx.request.body
