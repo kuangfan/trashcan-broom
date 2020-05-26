13aef8053e2b5893505b281b3b147597b08e7875
BengBu-YueZhang <zhangyue_coder@163.com>
2020-05-26 14:04:06 +0800

    feat: 更新sm.controller

--- trashcan-server/controller/sm.controller.js
+++ trashcan-server/controller/sm.controller.js
@@ -17,22 +17,30 @@ module.exports = {
   },
   async getSm (ctx, next) {
     const report = ctx.request.query
-    const { source, row, col } = report
-    const sourceFile = source.split('/')[source.split('/').length - 1]
-    const smFiles = fs.readdirSync(smPathDir)
-    const smFile = smFiles.filter(file => file.indexOf(sourceFile) > -1)
-    const smFilePath = path.resolve(__dirname, '../sm', smFile[0])
-    const smFileDataBuffer = fs.readFileSync(smFilePath)
-    const smFileDataStr = smFileDataBuffer.toString()
-    const smFileDataObj = JSON.parse(smFileDataStr)
-    const sources = smFileDataObj.sources.map(source => source.replace(/\.[\.\/]+/g, ''))
-    const consumer = await new sourceMap.SourceMapConsumer(smFileDataStr)
-    const result = consumer.originalPositionFor({
-      line: Number(row),
-      column: Number(col)
-    })
-    const index = sources.indexOf(result.source)
-    result.content = smFileDataObj.sourcesContent[index]
+    const { stack } = report
+    let result = ''
+    if (stack.length > 0) {
+      const smFiles = fs.readdirSync(smPathDir)
+      const source = stack[0].url
+      const row = stack[0].line
+      const col = stack[0].column
+      const sourceFile = source.split('/')[source.split('/').length - 1]
+      const smFile = smFiles.filter(file => file.indexOf(sourceFile) > -1)
+      if (smFile.length > 0) {
+        const smFilePath = path.resolve(__dirname, '../sm', smFile[0])
+        const smFileDataBuffer = fs.readFileSync(smFilePath)
+        const smFileDataStr = smFileDataBuffer.toString()
+        const smFileDataObj = JSON.parse(smFileDataStr)
+        const sources = smFileDataObj.sources.map(source => source.replace(/\.[\.\/]+/g, ''))
+        const consumer = await new sourceMap.SourceMapConsumer(smFileDataStr)
+        result = consumer.originalPositionFor({
+          line: Number(row),
+          column: Number(col)
+        })
+        const index = sources.indexOf(result.source)
+        result.content = smFileDataObj.sourcesContent[index]
+      }
+    }
     ctx.result = {
       msg: 'success',
       code: 200,
